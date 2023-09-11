5e4339615eaa3ed83e1dc9f03d60aba02bf7212f
Afreen <131018705+AfreenInnovates@users.noreply.github.com>
2023-09-11 09:02:54 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -104,7 +104,7 @@ const Post_List = {
       publish_date: "September 11, 2023",
       image_source:
         "https://images.pexels.com/photos/305568/pexels-photo-305568.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      post_destination: "/Posts/The-Debate-on-Universal-Free- Healthcare.html",
+      post_destination: "/Posts/The-Debate-on-Universal-Free-Healthcare.html",
     },
 		 {
       title: "Quantum Chemistry",
