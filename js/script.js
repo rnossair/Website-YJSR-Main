0b8a8f6153d97028076b1b3dfb0d09368c6b2154
AnonymousTechDeveloper <anshmalviya54@gmail.com>
2023-09-12 20:51:10 +0530

    (Temporarily and Partially) Removed An Article Title: Anthropogenic Habitat Alteration and the Demographic Decline of Scantius Aegyptius Author: Rithvik Sabnekar Reason: Author's request

--- js/script.js
+++ js/script.js
@@ -174,7 +174,7 @@ const Post_List = {
       post_destination: "/Posts/Precision-Oncology.html",
     },
 		{
-      title: "   HOW DOES BLOOD CLOT",
+      title: "HOW DOES BLOOD CLOT",
       subtitle: " HOW DO BLOOD COAGULATION TESTS WORK?",
       author: " Zehra Polat",
       author_contact: "",
@@ -444,15 +444,6 @@ const Post_List = {
 			"publish_date": "June 25, 2023",
 			"image_source": "https://images.unsplash.com/photo-1480044965905-02098d419e96?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
 			"post_destination": "/Posts/Global-warming-and-its-impact-on-the-phenology-of-birds-and-other-wildlife.html"
-		},
-		{
-			"title": "Anthropogenic Habitat Alteration and the Demographic Decline of Scantius Aegyptius",
-			"subtitle": "The Human Footprint on Scantius Aegyptius: Investigating the Ecological Consequences of Anthropogenic Habitat Alteration.",
-			"author": "Rithvik Sabnekar",
-			"author_contact": "https://www.linkedin.com/in/rithvik-sabnekar-1971a3266/",
-			"publish_date": "June 19, 2023",
-			"image_source": "../assets/thumbnails/anthropogenic_habitat_alteration_and_the_demographic_decline_of_scantius_aegyptius.png",
-			"post_destination": "/Posts/Anthropogenic-habitat-alteration-and-the-demographic-decline-of-scantius-aegyptius.html"
 		},
 		{
 			"title": "Rewiring Resilience",
