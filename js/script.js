34cb5ce936d498eb969ebb40bf1eafa1e0a368c7
Rayane Nossair <rnossairx@gmail.com>
2023-09-16 14:53:19 +0200

    new post: string theory influence on physics

--- js/script.js
+++ js/script.js
@@ -96,6 +96,15 @@ A format for the post list array items.
 const Post_List = {
 
 	"sci": [
+		{
+            title: "String Theory’s Influence on Modern Physics",
+            subtitle: "",
+            author: "Shreya Bose",
+            author_contact: "", // I personally dunno why does it even exists...
+            publish_date: "September 16, 2023",
+            image_source: "https://media.istockphoto.com/id/1305282522/photo/3d-render-abstract-background-with-colorful-spectrum-bright-pink-yellow-neon-rays-and-glowing.jpg?s=612x612&w=0&k=20&c=Xl0Ou3iYKojtEZG4bOwAstB-RYqn9UUzG6y1X36BVjs=",
+            post_destination: "/Posts/string_theory_influence_physics.html"
+        },
       {
             title: "Transforming Skin Cells into Pluripotent Marvels",
             subtitle: "",
