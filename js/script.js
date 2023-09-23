ea2cabafda9f712b3b08a2c0860767a0c7d8a821
AnonymousTechDeveloper <anshmalviya54@gmail.com>
2023-09-23 17:56:37 +0530

    Saved 213 articles lmao

--- js/script.js
+++ js/script.js
@@ -105,7 +105,7 @@ const Post_List = {
             image_source: "https://images.unsplash.com/photo-1578269174936-2709b6aeb913?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80",
             post_destination: "/Posts/The_Evolutionary_Psychology_of_Competition.html"
     },
-    }
+    {
 			title: "Unveiling the Mysteries: How Are Northern Lights Formed?",
 			subtitle: "The Enigmatic Dance of Electrons: Decoding the Celestial Light Show",
 			author: "Edbert Victor Fandy",
