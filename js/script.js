29a733e47a756dbfd7baf4601d9c7066c8282735
Afreen <131018705+AfreenInnovates@users.noreply.github.com>
2023-10-20 00:19:34 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -95,6 +95,16 @@ A format for the post list array items.
 
 const Post_List = {
 	"sci": [
+		 {
+      title: "Social media and teenage interpersonal relationships",
+      subtitle: "",
+      author: "Mohammed Ehsan Ullah Shareef & Grayson Mndeme",
+      publish_date: "October 20, 2023",
+      image_source:
+        "https://images.pexels.com/photos/2818118/pexels-photo-2818118.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      post_destination:
+        "/Posts/Social-media-and-teenage-interpersonal-relationships.html",
+    },
 		{
 			title: "The Case of Peroxidase",
 			subtitle: "A Study of Peroxidase Catalysis: Temperature, Concentration, and Spectrophotometry",
