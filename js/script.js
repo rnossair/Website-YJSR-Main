99c407ecfe72323ae89a8f8b007b487cd0d94199
Afreen <131018705+AfreenInnovates@users.noreply.github.com>
2023-11-01 21:40:53 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -95,6 +95,16 @@ A format for the post list array items.
 
 const Post_List = {
 	"sci": [
+		 {
+      title: "Infinity Paradoxes",
+      subtitle: "",
+      author: "Gülin Gürbüz",
+      publish_date: "November 1, 2023",
+      image_source:
+        "https://images.pexels.com/photos/4983247/pexels-photo-4983247.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+
+      post_destination: "Infinity-Paradoxes.html",
+    },
 		{
 			title: "Negative Effects of Pyrophosphate on Dinoflagellates",
 			subtitle: "",
